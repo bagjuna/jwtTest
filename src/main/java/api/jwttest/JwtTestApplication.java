e015df4b5e621c049fb55f016e035bdbe5064852
juna <juna3437@naver.com>
2025-01-15 08:40:13 +0900

    메인 -안녕하세요

--- src/main/java/api/jwttest/JwtTestApplication.java
+++ src/main/java/api/jwttest/JwtTestApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JwtTestApplication {
 
     public static void main(String[] args) {
+        System.out.println("안녕");
         SpringApplication.run(JwtTestApplication.class, args);
     }
 
